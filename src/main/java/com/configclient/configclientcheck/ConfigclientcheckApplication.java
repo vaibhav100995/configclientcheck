113ac7920df449371db36500a80f5709028f9ce0
vaibhav100995 <vaibhav100995@gmail.com>
2024-12-25 21:07:43 +0530

    config client project code added

---
+++ src/main/java/com/configclient/configclientcheck/ConfigclientcheckApplication.java
@@ -0,0 +1,13 @@
+package com.configclient.configclientcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigclientcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigclientcheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/configclient/configclientcheck/ConfigclientcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configclient.configclientcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigclientcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
