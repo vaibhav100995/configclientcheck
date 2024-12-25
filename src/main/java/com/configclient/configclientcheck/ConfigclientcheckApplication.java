a5d7d4dff06a541952c5b029042bf4ea4b177860
vaibhav100995 <vaibhav100995@gmail.com>
2024-12-25 21:31:01 +0530

    README file added

--- src/main/java/com/configclient/configclientcheck/ConfigclientcheckApplication.java
+++ src/main/java/com/configclient/configclientcheck/ConfigclientcheckApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfigclientcheckApplication {
 
+//	https://www.youtube.com/watch?v=yNnLICy2zk4
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigclientcheckApplication.class, args);
 	}
